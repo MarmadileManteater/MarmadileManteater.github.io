2599f8e74d3e6c620700d1a1ff19a168edb73184
Emma <MarmadileManteater@proton.me>
2024-05-29 14:43:11 -0400

    Update references to git repo

--- websites/personal-site/src/dataservice.js
+++ websites/personal-site/src/dataservice.js
@@ -11,4 +11,4 @@ export const blogPostRepository = fromDirectory('../../packages/static-data/post
 export const allTags = await getAllTagsFromJSONFile('../../packages/static-data/tags.json')
 export const allLinks = await getAllLinksFromJSONFile('../../packages/static-data/links.json')
 export const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
-export const sourceUrlBase = 'https://raw.githubusercontent.com/MarmadileManteater/MyMonorepo/tree/development/websites/personal-site/'
+export const sourceUrlBase = 'https://raw.githubusercontent.com/MarmadileManteater/MarmadileManteater.github.io/development/websites/personal-site/'
